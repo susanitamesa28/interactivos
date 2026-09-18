2d788b174b1b8248fb8cc82518892fcf99af0f76
Susana Mesa Mora <300974714+susanitamesa28@users.noreply.github.com>
2026-09-18 17:14:43 -0600

    Fix static route for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/interactivos",
   assetPrefix: "/interactivos/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
